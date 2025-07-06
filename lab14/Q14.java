75843b5fa0f8fb607d7122cacc63a04075b81f6e
sajin1208 <157976205+sajin1208@users.noreply.github.com>
2025-07-06 23:32:59 +0545

    Showing the use of URLEncoder Class

---
+++ lab14/Q14.java
@@ -0,0 +1,26 @@
+package lab14;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class Q14 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(URLEncoder.encode("This string has spaces", "UTF-8"));
+            System.out.println(URLEncoder.encode("This*string*has*asterisks", "UTF-8"));
+            System.out.println(URLEncoder.encode("This%string%has%percent%signs", "UTF-8"));
+            System.out.println(URLEncoder.encode("This+string+has+pluses", "UTF-8"));
+            System.out.println(URLEncoder.encode("This/string/has/slashes", "UTF-8"));
+            System.out.println(URLEncoder.encode("This\"string\"has\"quote\"marks", "UTF-8"));
+            System.out.println(URLEncoder.encode("This:string:has:colons", "UTF-8"));
+            System.out.println(URLEncoder.encode("This~string~has~tildes", "UTF-8"));
+            System.out.println(URLEncoder.encode("This(string)has(parentheses)", "UTF-8"));
+            System.out.println(URLEncoder.encode("This.string.has.periods", "UTF-8"));
+            System.out.println(URLEncoder.encode("This=string=has=equals=signs", "UTF-8"));
+            System.out.println(URLEncoder.encode("This&string&has&ampersands", "UTF-8"));
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException("Broken VM does not support UTF-8");
+        }
+    }
+    
+}
