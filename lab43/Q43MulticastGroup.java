2d1009b96b8f96fe5f1a1995e277f4b732280cb1
sajin1208 <157976205+sajin1208@users.noreply.github.com>
2025-07-07 23:15:43 +0545

    Illustrate the process for communicating with Multicast group

---
+++ lab43/Q43MulticastGroup.java
@@ -0,0 +1,111 @@
+package lab43;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class Q43MulticastGroup {
+    private static final String TERMINATE = "Exit"; 
+	static String name; 
+	static volatile boolean finished = false; 
+	public static void main(String[] args) 
+	{ 
+		if (args.length != 2)
+			System.out.println("Two arguments required: <multicast-host> <port-number>"); 
+		else{ 
+			try
+			{ 
+				InetAddress group = InetAddress.getByName(args[0]); 
+				int port = Integer.parseInt(args[1]); 
+                NetworkInterface networkInterface = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+				Scanner sc = new Scanner(System.in); 
+				System.out.print("Enter your name: "); 
+				name = sc.nextLine(); 
+				MulticastSocket socket = new MulticastSocket(port); 
+			
+				// Since we are deploying 
+				socket.setTimeToLive(0); 
+				//this on localhost only (For a subnet set it as 1) 
+				
+                socket.joinGroup(new InetSocketAddress(group, port), networkInterface);
+				Thread t = new Thread(new
+				ReadThread(socket,group,port)); 
+			
+				// Spawn a thread for reading messages 
+				t.start(); 
+				
+				// sent to the current group 
+				System.out.println("Start typing messages...\n"); 
+				while(true) 
+				{ 
+					String message; 
+					message = sc.nextLine(); 
+					if(message.equalsIgnoreCase(Q43MulticastGroup.TERMINATE)) 
+					{ 
+						finished = true; 
+                        socket.leaveGroup(new InetSocketAddress(group, port), networkInterface);
+						socket.close(); 
+						break; 
+					} 
+					message = name + ": " + message; 
+					byte[] buffer = message.getBytes(); 
+					DatagramPacket datagram = new
+					DatagramPacket(buffer,buffer.length,group,port); 
+					socket.send(datagram); 
+				} 
+			} 
+			catch(SocketException se) 
+			{ 
+				System.out.println("Error creating socket"); 
+				se.printStackTrace(); 
+			} 
+			catch(IOException ie) 
+			{ 
+				System.out.println("Error reading/writing from/to socket"); 
+				ie.printStackTrace(); 
+			} 
+		} 
+	} 
+} 
+class ReadThread implements Runnable 
+{ 
+	private MulticastSocket socket; 
+	private InetAddress group; 
+	private int port; 
+	private static final int MAX_LEN = 1000; 
+	ReadThread(MulticastSocket socket,InetAddress group,int port) 
+	{ 
+		this.socket = socket; 
+		this.group = group; 
+		this.port = port; 
+	} 
+	
+	@Override
+	public void run() 
+	{ 
+		while(!Q43MulticastGroup.finished) 
+		{ 
+				byte[] buffer = new byte[ReadThread.MAX_LEN]; 
+				DatagramPacket datagram = new
+				DatagramPacket(buffer,buffer.length,group,port); 
+				String message; 
+			try
+			{ 
+				socket.receive(datagram); 
+				message = new
+				String(buffer,0,datagram.getLength(),"UTF-8"); 
+				if(!message.startsWith(Q43MulticastGroup.name)) 
+					System.out.println(message); 
+			} 
+			catch(IOException e) 
+			{ 
+				System.out.println("Socket closed!"); 
+			} 
+		} 
+	} 
+}
